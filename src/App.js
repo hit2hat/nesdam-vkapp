3e92756714b9613f5c6b8b3693ec073c73abe44c
Stepan Novozhilov <hit2hat@icloud.com>
2019-05-28 18:04:51 +0300

    Add VKStories ❤

--- src/App.js
+++ src/App.js
@@ -2,41 +2,49 @@ import React, { useState } from 'react';
 import connect from "@vkontakte/vkui-connect-promise";
 import { View, Panel, PanelHeader, Input, FormLayout, FixedLayout, Button, ScreenSpinner, FormStatus } from '@vkontakte/vkui';
 
-const generateStory = (name, subject, callback) => {
-	const canvas = document.createElement("canvas");
-	canvas.width = 1080;
-	canvas.height = 1920;
+import VKStories from "vk-stories";
 
-	const ctx = canvas.getContext("2d");
-
-	ctx.textAlign = "center";
-	ctx.fillStyle = "#FFFFFF";
-
-	const background = new Image();
-	background.onload = () => {
-		ctx.drawImage(background, 0, 0);
-		ctx.font = "96px Arial Black";
-		ctx.fillText(name, 540, 1133);
-		ctx.font = "68px Arial";
-		ctx.fillText(subject, 540, 1494);
-		callback(canvas.toDataURL());
-	};
-	background.src = require("./assets/template.png");
-};
-
-const dataURLtoFile = (dataurl, filename) => {
-	let arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
-		bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
-	while(n--){
-		u8arr[n] = bstr.charCodeAt(n);
-	}
-	return new File([u8arr], filename, {type:mime});
-};
+// Init VKStories
+VKStories.init(connect);
 
 const App = () => {
 	const [ name, setName ] = useState("");
 	const [ subject, setSubject ] = useState("");
 	const [ status, setStatus ] = useState("none");
+
+	const makeStory = () => {
+		setStatus("making");
+		VKStories.generateStoryFromTemplate(require("./assets/template.png"), [
+			{
+				font: "96px Arial Black",
+				align: "center",
+				color: "#FFFFFF",
+				value: name,
+				x: 540,
+				y: 1133
+			},
+			{
+				font: "68px Arial",
+				align: "center",
+				color: "#FFFFFF",
+				value: subject,
+				x: 540,
+				y: 1494
+			}
+		])
+			.then((story) => {
+				VKStories.shareStory(6999763, story, { add_to_news: true })
+					.then(() => setStatus("shared"))
+					.catch((err) => {
+						console.error(err);
+						if (err.error_code === 1) {
+							setStatus("sharingError");
+						} else {
+							setStatus("error");
+						}
+					})
+			})
+	};
 
 	return (
 		<View activePanel="home" popout={status === "making" ? <ScreenSpinner/>: null}>
@@ -75,41 +83,7 @@ const App = () => {
 				<FixedLayout vertical="bottom" style={{ margin: 15, paddingRight: 30 }}>
 					<Button
 						size="xl"
-						onClick={() => {
-							setStatus("making");
-							generateStory(name, subject, (story) => {
-								connect.send("VKWebAppGetAuthToken", { app_id: 6999763, scope: "stories"})
-									.then((response) => {
-										const access_token = response.data.access_token;
-										connect.send("VKWebAppCallAPIMethod", {
-											method: "stories.getPhotoUploadServer",
-											params: {
-												access_token,
-												add_to_news: 1,
-												link_text: "open",
-												link_url: "https://vk.com/app6999763",
-												v: "5.95"
-											}
-										})
-											.then((response) => {
-												const uploadUrl = response.data.response.upload_url;
-
-												const request = new FormData();
-												request.append("file", dataURLtoFile(story, "story.png"));
-
-												fetch("https://cors-anywhere.herokuapp.com/" + uploadUrl, {
-													method: "POST",
-													body: request
-												})
-													.then(r => r.json())
-													.then(r => r.error ? setStatus("error") : setStatus("shared"))
-													.catch(console.error);
-											})
-											.catch(console.error)
-									})
-									.catch(() => setStatus("sharingError"));
-							});
-						}}
+						onClick={makeStory}
 					>
 						Опубликовать в истории
 					</Button>
